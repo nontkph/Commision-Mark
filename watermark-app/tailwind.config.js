96cbbb37646a3f00a403439e393b6a65856522d6
= <miuvtube@gmail.com>
2025-06-10 10:07:49 +0700

    Fix Sompackage

--- watermark-app/tailwind.config.js
+++ watermark-app/tailwind.config.js
@@ -1,12 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./utils/**/*.{js,ts,jsx,tsx}",
-    "./interfaces/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}; 
+}
