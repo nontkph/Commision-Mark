70ee18e21409fd49e144df358e13f58804b1addd
= <miuvtube@gmail.com>
2025-06-10 09:29:53 +0700

    first commit

---
+++ watermark-app/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./interfaces/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
